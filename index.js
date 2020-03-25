e974414710e2ca3385001747ff2fd6643fb4f659
unknown <alderjanio@hotmail.com>
2020-03-25 19:20:36 -0400

    testando portas

--- index.js
+++ index.js
@@ -37,5 +37,10 @@ app.get( '/inserir', async (req,res) => {
 })
 
 */
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 //server.connection({ port: process.env.PORT || 3000 }); 
-app.listen(process.env.PORT || 3000)
+//app.listen(process.env.PORT || 3000)
